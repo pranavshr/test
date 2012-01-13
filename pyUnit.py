5f5843d80230aa13c15b2cc5ea1304740de5be61
Pranav <pranav@uvic.ca>
2012-01-13 15:51:58 -0800

    A basic unit test

---
+++ pyUnit.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestSequence(unittest.TestCase):
+	def setUp(self):
+	#Gets called before EACH test
+		pass
+	def tearDown(self):
+	#Gets called after EACH test
+		pass
+
+	#The Tests:
+	def test_add_simple(self):
+		self.assertEqual(1, 1)
+
+if __name__=="__main__":
+	unittest.main()
+
